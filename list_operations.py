99ea08ba8679456badb1d3559bfd5a619f614846
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2021-07-23 04:08:19 +0000

    implemented inner four end

--- list_operations.py
+++ list_operations.py
@@ -101,7 +101,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -117,7 +117,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
